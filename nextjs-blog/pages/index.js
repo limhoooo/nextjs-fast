03efe7a2e12ee3f40179d6d286d5a0233aeb71e6
lim ho <dlagh123@gmail.com>
2023-04-22 17:50:04 +0900

    blog 프로젝트 push

---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/Layout'
+import utilStyles from '../styles/utils.module.css'
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>I love coding 😍</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  )
+}
